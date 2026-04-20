fbc204a26da6e98f0eb0ad734dae4a6d226822e9
Euler0525 <euler0525@139.com>
2026-04-20 19:41:01 +0800

    feat(windows): pwsh automatically saves logs

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,12 @@
 Set-Alias -Name lg -Value lazygit
+
+
+$logDir = "$env:USERPROFILE\.log"
+New-Item -Path $logDir -ItemType Directory -Force | Out-Null
+
+Get-ChildItem "$logDir\*.log" | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-30) } | Remove-Item -Force
+
+$logFile = Join-Path $logDir "pwsh_$(Get-Date -Format 'yyyyMMdd_HHmmss').log"
+Start-Transcript -Path $logFile | Out-Null
+
+Register-EngineEvent PowerShell.Exiting -Action { Stop-Transcript } | Out-Null
